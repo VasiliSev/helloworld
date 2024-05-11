d9615bea63e811a087612af4e052765094df9ac9
VasiliSev <v.salazko@gmail.com>
2024-05-11 16:12:52 +0300

    homework 8 task 1-4

--- src/Main.java
+++ src/Main.java
@@ -4,76 +4,76 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Задача 1");
-        int[] arrInt = new int[]{1, 2, 3};
-        float[] arrFloat = {1.57f, 7.654f, 9.986f};
-        long[] arrLong = new long[10];
-        for (int i = 0; i < arrLong.length; i++) {
-            arrLong[i] = (i + 1) * 5;
+
+        //Массив целочисленных данных
+        int[] arrayInt = new int[]{1, 2, 3};
+
+        //Массив данных с дробной частью - float
+        float[] arrayFloat = {1.57f, 7.654f, 9.986f};
+
+        //Произвольный массив - в данном случае, с данными типа Long
+        long[] arrayLong = new long[10];
+        for (int i = 0; i < arrayLong.length; i++) {
+            arrayLong[i] = (i + 1) * 5;
         }
         System.out.println();
 
         System.out.println("Задача 2");
         System.out.println("Массив 1 - целочисленный");
-        for (int i = 0; i < arrInt.length-1; i++) {
-            System.out.print(arrInt[i]+", ");
+        for (int i = 0; i < arrayInt.length-1; i++) {
+            System.out.print(arrayInt[i]+", ");
         }
-        System.out.print(arrInt[arrInt.length-1]);
+        System.out.print(arrayInt[arrayInt.length-1]);
         System.out.println();
         System.out.println("Массив 2 - с плавающей точкой");
-        for (int i = 0; i < arrFloat.length; i++) {
-             if (i == arrFloat.length-1) {
-                 System.out.println(arrFloat[i]);
-                 break;
-             }
-            System.out.print(arrFloat[i]+", ");
-            }
+        for (int i = 0; i < arrayFloat.length-1; i++) {
+            System.out.print(arrayFloat[i] + ", ");
+        }
+        System.out.println(arrayFloat[arrayFloat.length-1]);
         System.out.println();
         System.out.println("Массив 3 - произвольный");
-        for (int i = 0; i < arrLong.length; i++) {
-            if (i == arrLong.length-1){
-                System.out.println(arrLong[i]);
-                break;
-            }
-            System.out.print(arrLong[i]+", ");
+        for (int i = 0; i < arrayLong.length-1; i++) {
+            System.out.print(arrayLong[i]+", ");
         }
+        System.out.println(arrayLong[arrayLong.length-1]);
         System.out.println();
 
         System.out.println("Задача 3");
         System.out.println("Массив 1 в обратном порядке");
-        for (int i = arrInt.length-1; i >=0; i--) {
+        for (int i = arrayInt.length-1; i >=0; i--) {
             if (i == 0) {
-                System.out.println(arrInt[i]);
+                System.out.println(arrayInt[i]);
                 break;
             }
-            System.out.print(arrInt[i]+", ");
+            System.out.print(arrayInt[i]+", ");
         }
         System.out.println();
         System.out.println("Массив 2 в обратном порядке");
-        for (int i = arrFloat.length-1; i >=0; i--) {
+        for (int i = arrayFloat.length-1; i >=0; i--) {
             if (i == 0) {
-                System.out.println(arrFloat[i]);
+                System.out.println(arrayFloat[i]);
                 break;
             }
-            System.out.print(arrFloat[i]+", ");
+            System.out.print(arrayFloat[i]+", ");
         }
         System.out.println();
         System.out.println("Массив 3 в обратном порядке");
-        for (int i = 0; i < arrLong.length; i++) {
-            if (i == arrLong.length - 1) {
-                System.out.println(arrLong[arrLong.length - 1 - i]);
+        for (int i = arrayLong.length-1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.println(arrayLong[i]);
                 break;
             }
-            System.out.print(arrLong[arrLong.length - 1 - i] + ", ");
+            System.out.print(arrayLong[i] + ", ");
         }
         System.out.println();
 
         System.out.println("Задача 4");
-        for (int i = 0; i < arrInt.length; i++) {
-            if (arrInt[i] % 2 != 0){
-                arrInt[i] = arrInt [i]+1;
+        for (int i = 0; i < arrayInt.length; i++) {
+            if (arrayInt[i] % 2 != 0){
+                arrayInt[i] = arrayInt [i]+1;
             }
         }
-            System.out.println(Arrays.toString(arrInt));
+            System.out.println(Arrays.toString(arrayInt));
     }
     }
 
