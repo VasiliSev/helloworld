82ee8e07fc42c88d9b9c59634ead0ce4b997b6e9
VasiliSev <v.salazko@gmail.com>
2024-05-16 21:21:32 +0300

    homework 9 tasks 1-4

--- src/Main.java
+++ src/Main.java
@@ -5,75 +5,59 @@ public class Main {
 
         System.out.println("Задача 1");
 
-        //Массив целочисленных данных
-        int[] arrayInt = new int[]{1, 2, 3};
-
-        //Массив данных с дробной частью - float
-        float[] arrayFloat = {1.57f, 7.654f, 9.986f};
-
-        //Произвольный массив - в данном случае, с данными типа Long
-        long[] arrayLong = new long[10];
-        for (int i = 0; i < arrayLong.length; i++) {
-            arrayLong[i] = (i + 1) * 5;
+        //Произвольный массив для первой задачи
+        int[] arrayOne = new int[]{10000, 22000, 37000, 18000, 28000};
+        int sum1 = 0;
+        for (int j : arrayOne) {
+            sum1 += j;
         }
+        System.out.println("Сумма трат за месяц составила "+sum1);
         System.out.println();
 
         System.out.println("Задача 2");
-        System.out.println("Массив 1 - целочисленный");
-        for (int i = 0; i < arrayInt.length-1; i++) {
-            System.out.print(arrayInt[i]+", ");
+
+        //Произвольный массив для второй задачи
+        int[] arrayTwo = new int[5];
+        arrayTwo[0] = 25000;
+        arrayTwo[1] = 38000;
+        arrayTwo[2] = 42000;
+        arrayTwo[3] = 35000;
+        arrayTwo[4] = 78000;
+
+        int min = 100000;
+        int max = 0;
+        for (int element : arrayTwo) {
+            if (element < min) {
+                min = element;
+            }
+            if (element > max) {
+                max = element;
+            }
         }
-        System.out.print(arrayInt[arrayInt.length-1]);
-        System.out.println();
-        System.out.println("Массив 2 - с плавающей точкой");
-        for (int i = 0; i < arrayFloat.length-1; i++) {
-            System.out.print(arrayFloat[i] + ", ");
-        }
-        System.out.println(arrayFloat[arrayFloat.length-1]);
-        System.out.println();
-        System.out.println("Массив 3 - произвольный");
-        for (int i = 0; i < arrayLong.length-1; i++) {
-            System.out.print(arrayLong[i]+", ");
-        }
-        System.out.println(arrayLong[arrayLong.length-1]);
+        System.out.println("Минимальная трата за месяц составила "+min+", максимальная - "+max);
         System.out.println();
 
         System.out.println("Задача 3");
-        System.out.println("Массив 1 в обратном порядке");
-        for (int i = arrayInt.length-1; i >=0; i--) {
-            if (i == 0) {
-                System.out.println(arrayInt[i]);
-                break;
-            }
-            System.out.print(arrayInt[i]+", ");
+
+        //Произвольный массив для третьей задачи
+        int[] arrayThree = new int[]{39000, 48000, 94000, 62000, 71000};
+        int numbersOfWeeks = 5;
+        int sum3 = 0;
+        for (int element : arrayThree) {
+            sum3 += element;
         }
-        System.out.println();
-        System.out.println("Массив 2 в обратном порядке");
-        for (int i = arrayFloat.length-1; i >=0; i--) {
-            if (i == 0) {
-                System.out.println(arrayFloat[i]);
-                break;
-            }
-            System.out.print(arrayFloat[i]+", ");
-        }
-        System.out.println();
-        System.out.println("Массив 3 в обратном порядке");
-        for (int i = arrayLong.length-1; i >= 0; i--) {
-            if (i == 0) {
-                System.out.println(arrayLong[i]);
-                break;
-            }
-            System.out.print(arrayLong[i] + ", ");
-        }
+        float averageWaste = (float) sum3 /numbersOfWeeks;
+        String result = String.format("%.2f",averageWaste);
+        System.out.println("Средняя сумма трат составила "+result);
         System.out.println();
 
         System.out.println("Задача 4");
-        for (int i = 0; i < arrayInt.length; i++) {
-            if (arrayInt[i] % 2 != 0){
-                arrayInt[i] = arrayInt [i]+1;
-            }
+        char[] reverseFullName = {'n','a','v','I',' ', 'v','o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length-1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
         }
-            System.out.println(Arrays.toString(arrayInt));
+        System.out.println();
+
     }
     }
 
