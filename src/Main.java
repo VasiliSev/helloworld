b230c041965b9a642b34535668c5b4cf958fe033
VasiliSev <v.salazko@gmail.com>
2024-05-20 22:34:34 +0300

    Homework 10 tasks 1-3

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,26 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Задача 1 ");
-        String firstName = "Ivan ";
-        String middleName = "Ivanovich ";
-        String lastName = "Ivanov ";
-        String fullName = lastName + firstName + middleName;
-        System.out.println("ФИО сотрудника - " + fullName);
+          String firstName = "Ivan ";
+          String middleName = "Ivanovich ";
+          String lastName = "Ivanov ";
+          String fullName = lastName + firstName + middleName;
+          System.out.println("Ф. И. О. сотрудника - " + fullName);
+
         System.out.println();
 
         System.out.println("Задача 2");
         //В соответствии с условиями, в качестве исходных данных используем строку fullName с данными
         //Ivanov Ivan Ivanovich.
-        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета - " + fullName.toUpperCase());
+          System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета - " + fullName.toUpperCase());
+
         System.out.println();
 
         System.out.println("Задача 3");
-        String fullName3 = "Иванов Семён Семёнович";
-        String fullName3Changed = fullName3.replace('ё', 'e');
-        System.out.println("Данные Ф.И.О. сотрудника - " + fullName3Changed);
-
+          String fullName3 = "Иванов Семён Семёнович";
+          String fullName3Changed = fullName3.replace('ё', 'e');
+          System.out.println("Данные Ф. И. О. сотрудника - " + fullName3Changed);
     }
-
 }
 
 
