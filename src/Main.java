534c90b36044e7435b033c866c0380172e266838
VasiliSev <v.salazko@gmail.com>
2024-05-24 20:02:31 +0300

    Homework 11 tasks 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,62 @@
 public class Main {
+
+    //метод для задачи 1
+    public static void checkYearToLeap(int year) {
+        if (year < 1584) {
+            System.out.println(year + " год относится к эпохе, когда не было понятия високосный год");
+        } else if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+    }
+
+    //метод для задачи 2
+    public static void choiceOfOperationSystem(int year, byte OS) {
+        if (OS == 1) {
+            if (year < 2015) {
+                System.out.println("Установите облегченную версию приложения для ОС Android");
+            } else {
+                System.out.println("Установите полную версию приложения для ОС Android");
+            }
+        } else if (year < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS");
+        } else {
+            System.out.println("Установите полную версию приложения для iOS");
+        }
+    }
+
+    //метод для задачи 3
+    public static int daysForDelivery(int dist) {
+        int time = 1;
+        if (dist > 100) {
+            return -1;
+        } else if (dist <= 20) {
+            return time;
+        } else if (dist <= 60) {
+            return time + 1;
+        } else return time + 2;
+    }
+
     public static void main(String[] args) {
 
-        System.out.println("Задача 1 ");
-          String firstName = "Ivan ";
-          String middleName = "Ivanovich ";
-          String lastName = "Ivanov ";
-          String fullName = lastName + firstName + middleName;
-          System.out.println("Ф. И. О. сотрудника - " + fullName);
+        System.out.println("Задача 1 Методы часть 1");
+        int checkYear = 1980;
+        checkYearToLeap(checkYear);
 
         System.out.println();
 
-        System.out.println("Задача 2");
-        //В соответствии с условиями, в качестве исходных данных используем строку fullName с данными
-        //Ivanov Ivan Ivanovich.
-          System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета - " + fullName.toUpperCase());
+        System.out.println("Задача 2 Методы часть 1");
+        int yearOfProductionClientDevice = 2024;
+        byte clientOS = 1;
+        choiceOfOperationSystem(yearOfProductionClientDevice, clientOS);
 
         System.out.println();
 
-        System.out.println("Задача 3");
-          String fullName3 = "Иванов Семён Семёнович";
-          String fullName3Changed = fullName3.replace('ё', 'e');
-          System.out.println("Данные Ф. И. О. сотрудника - " + fullName3Changed);
-
+        System.out.println("Задача 3 Методы часть 1");
+        int myDistance = 70;
+        int timeOfDelivery = daysForDelivery(myDistance);
+        System.out.println("Время доставки составляет " + timeOfDelivery + " дней (-1 - доставка не осуществляется)");
     }
 }
 
