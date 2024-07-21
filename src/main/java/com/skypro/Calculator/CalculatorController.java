08cd1dc30c52c40510c9196d0687a750c8b23b7b
VasiliSev <v.salazko@gmail.com>
2024-07-21 18:49:15 +0300

    Введение в Maven и SpringBoot

--- src/main/java/com/skypro/Calculator/CalculatorController.java
+++ src/main/java/com/skypro/Calculator/CalculatorController.java
@@ -21,44 +21,44 @@ public class CalculatorController {
     }
 
     @GetMapping("/plus")
-    public String plus (@RequestParam (required = false) Integer num1, @RequestParam (required = false) Integer num2) {
-        if (num1 == null ) {
-      return "Введите первое слагаемое";
+    public String plus(@RequestParam(required = false) Integer num1, @RequestParam(required = false) Integer num2) {
+        if (num1 == null) {
+            return "Введите первое слагаемое" ;
         } else if (num2 == null) {
-            return "Введите второе слагаемое";
+            return "Введите второе слагаемое" ;
         }
         int result = calculatorService.add(num1, num2);
         return num1 + "+" + num2 + "=" + result;
     }
 
     @GetMapping("/minus")
-    public String minus (@RequestParam (required = false) Integer num1, @RequestParam (required = false) Integer num2) {
-        if (num1 == null ) {
-            return "Введите уменьшаемое";
+    public String minus(@RequestParam(required = false) Integer num1, @RequestParam(required = false) Integer num2) {
+        if (num1 == null) {
+            return "Введите уменьшаемое" ;
         } else if (num2 == null) {
-            return "Введите вычитаемое";
+            return "Введите вычитаемое" ;
         }
         int result = calculatorService.sub(num1, num2);
         return num1 + "-" + num2 + "=" + result;
     }
 
     @GetMapping("/multiply")
-    public String multiply (@RequestParam (required = false) Integer num1, @RequestParam (required = false) Integer num2) {
+    public String multiply(@RequestParam(required = false) Integer num1, @RequestParam(required = false) Integer num2) {
         if (num1 == null) {
-            return "Введите первый множитель";
+            return "Введите первый множитель" ;
         } else if (num2 == null) {
-            return "Введите второй множитель";
+            return "Введите второй множитель" ;
         }
         int result = calculatorService.multiplication(num1, num2);
         return num1 + "*" + num2 + "=" + result;
     }
 
     @GetMapping("/divide")
-    public String divide (@RequestParam (required = false) Integer num1, @RequestParam (required = false) Integer num2) {
+    public String divide(@RequestParam(required = false) Integer num1, @RequestParam(required = false) Integer num2) {
         if (num1 == null) {
-            return "Введите делимое";
+            return "Введите делимое" ;
         } else if (num2 == null) {
-            return "Введите делитель";
+            return "Введите делитель" ;
         }
         if (num2 == 0) {
             return "Введите значение делителя, отличное от нуля" ;
