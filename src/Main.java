6526fca17da7778974e6b09820926c48b606c271
VasiliSev <v.salazko@gmail.com>
2024-07-29 01:10:51 +0300

    Exceptions

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println((int)'0');
-try {
-    CheckPasswordAndLogin.checkLoginPassword("Vsz_1234", "123_9871", "123_987");
-    System.out.println("Все отлично!");
-} catch (WrongLoginException e){
-    System.err.println("Error!" + e.getMessage());
-    }
-catch (WrongPasswordException e) {
-    System.err.println("Error!" + e.getMessage());
-}
-finally {
-    System.out.println("Работа закончена");
-}
+
+        try {
+            CheckPasswordAndLogin.checkLoginPassword("Vsz_1234", "123_9871", "123_987");
+            System.out.println("Все отлично!");
+        } catch (WrongLoginException e) {
+            System.err.println("Error!" + e.getMessage());
+        } catch (WrongPasswordException e) {
+            System.err.println("Error!" + e.getMessage());
+        } finally {
+            System.out.println("Работа закончена");
+        }
     }
 }
 
